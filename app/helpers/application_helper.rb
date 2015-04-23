de778910b53a9b74e95dc8f1ef25b26610a7d219
Nick LaPierre <lapierreni@gmail.com>
2015-04-23 13:12:20 +0000

    Begins rails flavored Ruby Chapter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  #Returns the full title or default title
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sampl App"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
+  def setup
+    @title_variable = "| Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home #{@title_variable}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help #{@title_variable}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About #{@title_variable}"
   end
 end
