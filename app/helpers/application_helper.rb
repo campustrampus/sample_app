b75ec38af49a9a3d53a2e71b6219c0ac8cba1aa0
Nick <lapierreni@gmail.com>
2015-05-22 16:42:18 -0400

    Finish layout and begin sign in page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   #Returns the full title or default title
   def full_title(page_title = '')
-    base_title = "Ruby on Rails Tutorial Sampl App"
+    base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
       base_title
     else
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -23,4 +23,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About #{@title_variable}"
   end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact #{@title_variable}"
+  end
 end
